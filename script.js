1578f3203bdb89b6363feed0830d678a91c0b387
valeria-hub1 <frotavaleria967@gmail.com>
2026-05-19 10:18:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 function entrar(event) {
-  event.preventDefault();
-  window.location.href = "medico.html";
+
+    event.preventDefault();
+
+    alert("Login realizado!");
+
+    window.location.href = "medico.html";
+}
+
+function editar() {
+    alert("Editar cadastro");
+}
+
+function confirmarExclusao() {
+
+    let resposta = confirm(
+        "Tem certeza que deseja apagar este item?"
+    );
+
+    if (resposta == true) {
+        alert("Item apagado com sucesso!");
+    } else {
+        alert("Operação cancelada!");
+    }
 }
